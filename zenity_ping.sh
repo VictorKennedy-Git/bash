fdc7df04996fecefa4a78560cd98a00e18c80ad3
victorkennedy-git <victorsilvasm92@gmail.com>
2025-01-08 17:20:16 -0300

    Organizando

--- zenity_ping.sh
+++ zenity_ping.sh
@@ -28,8 +28,98 @@ case $? in
         timeout=$(expr "$tempo_teste" \* 60 \* 60)
       ;;
       OUTRO)
-        valor=$(zenity --forms --add-entry="Valor tempo" --add-combo="Tipo" --combo-values="Horas|Minutos|Segundos")
-        exit 1
+        tempofuncao(){
+            valor=$(zenity --forms --add-entry="Valor tempo" --add-combo="Tipo" --combo-values="Horas|Minutos|Segundos")    
+            case $? in
+                0)
+                    data=($(echo $valor | tr "|" " "))
+                    if [[ ! ${data[0]} =~ ^[0-9]+$ ]]; then
+                        repeat=$(zenity --question --icon-name=dialog-error --text="Informações incorretas\n<b>\nDeseja continuar?</b>")
+                            case $? in
+                                0)
+                                    data=()
+                                    tempofuncao
+                                ;;
+                                1)
+                                    zenity --info --text="Cancelado Execução"
+                                    sleep 4
+                                    exit 1
+                                ;;
+                                -1)
+                                    zenity --info --text="Erro inesperado"
+                                ;;
+                            esac
+                        exit 1
+                    fi
+                ;;
+                1)
+                    zenity --info --text="Cancelado"
+                    exit 1
+                ;;
+                -1)
+                    validador="Erro"
+                ;;
+            esac    
+            if [[ $validador -eq 0 ]] && [[ ${#data[@]} -eq 2 ]]; then  
+                zenity --info --text="Preenchido Corretamente"
+            elif [[ $valioador -eq 1 ]]; then
+                zenity --info --text="Cancelado Execução"
+            else
+                while [[ ${#data[@]} -lt 2 ]]; do
+                    
+                    valor=$(zenity --forms --text="Por favor preencha os campos" --add-entry="Valor tempo" --add-combo="Tipo" --combo-values="Horas|Minutos|Segundos")
+                
+                    case $? in
+                        0)
+                            data=($(echo $valor | tr "|" " "))
+                            if [[ ! ${data[0]} =~ ^[0-9]+$ ]]; then
+                                repeat=$(zenity --question --icon-name=dialog-error --text="Informações incorretas\n<b>\nDeseja continuar?</b>")
+                                    case $? in
+                                        0)
+                                            data=()
+                                            tempofuncao
+                                        ;;
+                                        1)
+                                            zenity --info --text="Cancelado Execução"
+                                            sleep 4
+                                            exit 1
+                                        ;;
+                                        -1)
+                                            zenity --info --text="Erro inesperado"
+                                        ;;
+                                    esac
+                                exit 1
+                            fi
+                        ;;
+                        1)
+                            zenity --info --text="Cancelado"
+                            exit 1
+                        ;;
+                        -1)
+                            zenity --info --text="Erro inesperado"
+                        ;;
+                    esac
+                done
+            fi    
+        }
+
+        tempofuncao
+
+        case ${data[1]} in
+            "Horas")
+                timeout=$(expr ${data[0]} \* 60 \* 60)
+                echo $timeout
+            ;;
+            "Minutos")
+                timeout=$(expr ${data[0]} \* 60)
+                echo $timeout
+            ;;
+            "Segundos")
+                timeout=$(expr ${data[0]})
+                echo $timeout
+            ;;
+        esac
+
       ;;
     esac
   ;;
@@ -101,6 +191,9 @@ case $? in
             ;;
             24)
                 t="24 Horas"
+            ;;
+            OUTRO)
+                t=$(echo ${data[@]})
             ;;
         esac
 
